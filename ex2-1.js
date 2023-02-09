5feb2e37648543178ba1e28bc9eeb0e18f67b7d1
aruru2 <104402628+aruru2@users.noreply.github.com>
2023-02-10 03:05:53 +0900

    Create ex2-1.js

---
+++ ex2-1.js
@@ -0,0 +1,8 @@
+function imgchange1(){
+  document.getElementById('logo2').src=
+    './image/FUKASE2.jpg';}
+function imgchange2(){
+  document.getElementById('logo2').src=
+    './image/FUKASE.jpg';
+  setTimeout("imgchange4()",3000);
+}
