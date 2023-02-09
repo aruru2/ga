374857c5012a3b565dfa3e9d7b8fffe386983840
aruru2 <104402628+aruru2@users.noreply.github.com>
2023-02-10 03:08:37 +0900

    Create ex2-4.js

---
+++ ex2-4.js
@@ -0,0 +1,8 @@
+function imgchange7(){
+  document.getElementById('logo2').src=
+    './image/DJ2.jpg';}
+function imgchange8(){
+  document.getElementById('logo2').src=
+    './image/DJ.jpg';
+  setTimeout("imgchange4()",3000);
+}
