f757015717e04f74390ab6aeaf817958103303cf
aruru2 <104402628+aruru2@users.noreply.github.com>
2023-02-10 03:07:01 +0900

    Create ex2-2.js

---
+++ ex2-2.js
@@ -0,0 +1,8 @@
+function imgchange3(){
+  document.getElementById('logo2').src=
+    './image/SAORI2.jpg';}
+function imgchange4(){
+  document.getElementById('logo2').src=
+    './image/SAORI.jpg';
+  setTimeout("imgchange4()",3000);
+}
