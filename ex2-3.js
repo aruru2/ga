d3e35c5631adfd9601fc9094b560783037002dbd
aruru2 <104402628+aruru2@users.noreply.github.com>
2023-02-10 03:07:55 +0900

    Create ex2-3.js

---
+++ ex2-3.js
@@ -0,0 +1,8 @@
+function imgchange5(){
+  document.getElementById('logo2').src=
+    './image/NAZIN2.jpg';}
+function imgchange6(){
+  document.getElementById('logo2').src=
+    './image/NAKAZIN.jpg';
+  setTimeout("imgchange4()",3000);
+}
