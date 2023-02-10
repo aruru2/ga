345718a7fdc7e90a8cc868c1e1400f1f7ad8546a
aruru2 <104402628+aruru2@users.noreply.github.com>
2023-02-10 21:12:17 +0900

    Update ex2-1.js

--- ex2-1.js
+++ ex2-1.js
@@ -1,2 +1,2 @@
-function imgchange1(){document.getElementById('logo').src='./image/FUKASE2.jpg';}
+function imgchange1(){document.getElementById('logo').src='./image/FUKASW2.jpg';}
 function imgchange2(){document.getElementById('logo').src='./image/FUKASE.jpg';}
