4076971cc45785cfab7db4689ad179043e1b15e2
aruru2 <104402628+aruru2@users.noreply.github.com>
2023-02-10 20:52:19 +0900

    Update ex2-2.js

--- ex2-2.js
+++ ex2-2.js
@@ -1,10 +1,2 @@
-function imgchange3(){
-  document.getElementById('logo2').src=
-    './image/SAORI2.jpg';}
-function imgchange4(){
-  document.getElementById('logo2').src=
-    './image/SAORI.jpg';
-  setTimeout("imgchange4()",3000);
-}
 function imgchange3(){document.getElementById('logo').src='./image/SAORI2.jpg';}
 function imgchange4(){document.getElementById('logo').src='./image/SAORI.jpg';}
