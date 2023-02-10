3e84c280f15b4279bd0882f47f528106c263124c
aruru2 <104402628+aruru2@users.noreply.github.com>
2023-02-10 20:54:11 +0900

    Update ex2-4.js

--- ex2-4.js
+++ ex2-4.js
@@ -1,8 +1,2 @@
-function imgchange7(){
-  document.getElementById('logo2').src=
-    './image/DJ2.jpg';}
-function imgchange8(){
-  document.getElementById('logo2').src=
-    './image/DJ.jpg';
-  setTimeout("imgchange4()",3000);
-}
+function imgchange7(){document.getElementById('logo').src='./image/DJ2.jpg';}
+function imgchange8(){document.getElementById('logo').src='./image/DJ.jpeg';}
