876600e087dcd0de6eb1f010fdeda43f5042817a
aruru2 <104402628+aruru2@users.noreply.github.com>
2023-02-10 20:53:17 +0900

    Update ex2-3.js

--- ex2-3.js
+++ ex2-3.js
@@ -1,8 +1,2 @@
-function imgchange5(){
-  document.getElementById('logo2').src=
-    './image/NAZIN2.jpg';}
-function imgchange6(){
-  document.getElementById('logo2').src=
-    './image/NAKAZIN.jpg';
-  setTimeout("imgchange4()",3000);
-}
+function imgchange5(){document.getElementById('logo').src='./image/NAZIN2.jpg';}
+function imgchange6(){document.getElementById('logo').src='./image/NAKAZIN.jpg';}
